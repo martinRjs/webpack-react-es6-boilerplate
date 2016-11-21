b23475ec642455affcd6b24681389bf68b5a56c5
Martin Rojas <marteen.3107@gmail.com>
2016-11-20 19:04:41 -0600

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src/app.jsx'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders:[{
+			test: /\.js$/,
+			exclude:/node_modules/,
+			include: [
+      			path.resolve(__dirname, "/node_modules/"),
+    		],
+			loader: 'babel-loader',
+			query: {
+ 				 'presets': ['es2015']
+			}
+		},{
+			test: /\.jsx$/,
+    		loader: 'babel',
+    		exclude:/node_modules/,
+			include: [__dirname + '/node_modules', __dirname + '/src/'],
+		}]
+	}
+}
